1ff4af4a6ab729385b4fbdb41ed53d6ae3f34e85
jetflare <kunchyi1@gmail.com>
2016-09-26 23:40:20 +0800

    Update calc.js

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,6 @@
 var atmPressure = 101325 // Pa
 var operatingPressureAbs = 10 * 1e2 * 1e3 ; //bar (convert bar to Pa)
-var designPressure = (operatingPressure - atmPressure) * 1.1
+var designPressure = (operatingPressureAbs - atmPressure) * 1.1
 
 var innerDiameter = 2 // m
 var maxAllowStress = 135 * 1e3 * 1e3 // (convert from N/mm^2 to Pa)
@@ -13,5 +13,5 @@ var thickDenom2 = 4*maxAllowStress*jointEfficiency+.8*designPressure
 var thickness1 = thickNumer/thickDenom1
 var thickness2 = thickNumer/thickDenom2
 var minThickness = Math.max(thickness1,thickness2)
-// display minThickness in mm: minThickness*1e-3
+// display minThickness in mm: minThickness*1e3
 
