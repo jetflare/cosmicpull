89bfc1cde86485134069ed7ad2053997bd1fb03b
jetflare <kunchyi1@gmail.com>
2016-09-30 13:56:45 +0800

    Update input.js

--- scripts/input.js
+++ scripts/input.js
@@ -1,9 +1,15 @@
 function createInput(frontString,idString,value,backString){
 	var line = document.createElement("p")
 	document.getElementById("inputList").appendChild(line)
-	line.innerHTML = frontString + ": <input type=<q>text</q> class=<q>inputText</q> id=<q>"
-	line += idString + "</q> value=<q>"
-	line += value + "</q>> " + backString + "<br>"
+	line.innerHTML = frontString + ": "
+	
+	var inputText = document.createElement("input")
+	inputText.setAttribute("type","text")
+	inputText.setAttribute("id",idString)
+	inputText.setAttribute("value",value)
+	line.appendChild(inputText)
+	
+	line.innerHTML += backString + "<br>"
 }
 
 createInput("Height, between tangent lines","height",50,"m")
